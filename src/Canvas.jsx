decc76e1e200980b9e510ca85fac3f98ffcc0971
dibkb <dibas9110@gmail.con>
2022-01-21 00:48:27 +0530

    first commit

---
+++ src/Canvas.jsx
@@ -0,0 +1,60 @@
+// css
+import "./Canvas.css";
+import React, { useState, useRef, useEffect, useCallback } from "react";
+const Canvas = () => {
+  const canvasRef = useRef(null);
+  const divRef = useRef(null);
+  const chipRef = useRef(null);
+  const [marginTop, setMarginTop] = useState(0);
+  // on first render reposition canvas to fit size of div
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.style.top = `${divRef.current.getBoundingClientRect().top}px`;
+    canvas.style.left = `${divRef.current.getBoundingClientRect().left}px`;
+    canvas.style.height = `${divRef.current.getBoundingClientRect().height}px`;
+    canvas.style.width = `${divRef.current.getBoundingClientRect().width}px`;
+  }, []);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    divRef.current.style.top = `${marginTop}px`;
+    let heightToIncrease =
+      divRef.current.getBoundingClientRect().bottom -
+      canvasRef.current.getBoundingClientRect().bottom;
+    canvasRef.current.style.height = `${
+      canvasRef.current.getBoundingClientRect().bottom + heightToIncrease
+    }px`;
+    const center = canvas.getBoundingClientRect().width / 2;
+    if (context) {
+      context.beginPath();
+      context.moveTo(center, 0);
+      context.lineTo(
+        center,
+        canvas.getBoundingClientRect().bottom - canvas.getBoundingClientRect().y
+      );
+      context.strokeStyle = "#1087e8";
+      context.stroke();
+    }
+    //  console.log(divRef.current.getBoundingClientRect());
+  }, [marginTop]);
+  return (
+    <div className="container">
+      <canvas ref={canvasRef} className="canvas"></canvas>
+      <div className="content" ref={divRef}>
+        <div className="chip" ref={chipRef}>
+          Margin Top : {marginTop}px
+        </div>
+      </div>
+
+      <div className="input">
+        Change Top Margin
+        <input
+          type="number"
+          onChange={useCallback((e) => setMarginTop(e.target.value), [])}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default React.memo(Canvas);
