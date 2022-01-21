0663d2f05dab453ae40469297550ba9a3634ae40
dibkb <dibas9110@gmail.con>
2022-01-21 13:45:26 +0530

    used canvas for only the marginTop

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -11,27 +11,30 @@ const Canvas = () => {
     const canvas = canvasRef.current;
     canvas.style.top = `${divRef.current.getBoundingClientRect().top}px`;
     canvas.style.left = `${divRef.current.getBoundingClientRect().left}px`;
-    canvas.style.height = `${divRef.current.getBoundingClientRect().height}px`;
+    canvas.style.height = `${divRef.current.getBoundingClientRect().top}px`;
     canvas.style.width = `${divRef.current.getBoundingClientRect().width}px`;
   }, []);
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    divRef.current.style.top = `${marginTop}px`;
-    let heightToIncrease =
-      divRef.current.getBoundingClientRect().bottom -
-      canvasRef.current.getBoundingClientRect().bottom;
+    divRef.current.style.marginTop = `${marginTop}px`;
     canvasRef.current.style.height = `${
-      canvasRef.current.getBoundingClientRect().bottom + heightToIncrease
+      divRef.current.getBoundingClientRect().top
     }px`;
+
+    // console.log(heightToIncrease);
     const center = canvas.getBoundingClientRect().width / 2;
     if (context) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
       context.beginPath();
       context.moveTo(center, 0);
-      context.lineTo(
-        center,
-        canvas.getBoundingClientRect().bottom - canvas.getBoundingClientRect().y
-      );
+      context.lineTo(center, 100);
+      console.log(canvas.getBoundingClientRect().bottom);
+
+      // context.lineTo(
+      //   center,
+      //   canvas.getBoundingClientRect().bottom - canvas.getBoundingClientRect().y
+      // );
       context.strokeStyle = "#1087e8";
       context.stroke();
     }
